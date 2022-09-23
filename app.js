fae0473072cfa9760c099af9e1b8a4eebf0f0cb5
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2022-09-24 00:24:32 +0600

    user authentication and sign up process

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const cors = require('cors');//If frontend and backend servers are different.
+const userRouter = require('./routers/userRouter');
+const morgan = require('morgan');
 
 //Cross Origin Resource Sharing
 
 const app = express();
 
+if (process.env.NODE_ENV === 'development') {
+    console.log("development server");
+    app.use(morgan('dev'));
+}
 app.use(cors());
 app.use(express.json()); // POST -> .json -> req.body
+
+
+app.use('/api/user', userRouter);
 
 module.exports = app;
