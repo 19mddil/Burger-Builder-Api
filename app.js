d53ab17f09893b3c8c4d84df7ab257ec5fb8fbc6
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2022-09-26 00:10:07 +0600

    connected to atlas and prepared for heroku

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');//If frontend and backend servers are different.
+const compression = require('compression');
 const userRouter = require('./routers/userRouter');
 const orderRouter = require('./routers/orderRouter');
 const morgan = require('morgan');
@@ -12,6 +13,8 @@ if (process.env.NODE_ENV === 'development') {
     console.log("development server");
     app.use(morgan('dev'));
 }
+
+app.use(compression());
 app.use(cors());
 app.use(express.json()); // POST -> .json -> req.body
 
